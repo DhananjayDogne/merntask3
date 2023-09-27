9ba8b58cd0220a3eb81238406ad51a6f96c4b380
dhananjay <dhananjaydogne@gmail.com>
2023-09-28 00:02:41 +0530

    adding all files

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,7 +15,7 @@ const Profile = () => {
             {ploader && <MoonLoader color="#36d7b7" className="m-auto" />}
             {user ? (
                 <div className="m-auto w-[70%]">
-                    <div className=" relative my-2 h-[12rem] border-[1px] rounded-full">
+                    <div className="m-auto relative my-2 h-[12rem] w-[12rem] border-[1px] rounded-full">
                         {/* <img className="backimg m-auto absolute  -z-[1]  left-[6.9rem]  my-2 w-[10rem] rounded-full" src="https://s3-alpha-sig.figma.com/img/07cc/91a0/2d7dfefdef2a26a5e2aae42f44491f3f?Expires=1696809600&Signature=BV9y7tiYFp~PJ0YN8yVMUIbGz7YRwp83GkRjSfwq61-Z3FtARHk7uMniNIWFOKFsDlJi0JLbW2PgU931bzQwm54e7YSg0e8Ai34b9iH33kTs1iDliaAIkuioGhxfK2UG6MpfxSNJHVBVtSdcsKZ6O0nX5XAfhQZFqv6MfzneIiWcCcKevVARS1F44pnsz2YWTQ5F1vGu7LCiv7C1loC5AMa8EZxOf1mqh-PnKU2YdBNuv3Y2kZ4aUnYU4DgmnzcRqwYa4tYeI8oVemYZSIA6WetqjkxUejdtKXaYQYapch79QXglPk79-bCcC~QDi2iQlzuUBZK3A4dxC6BXdKofPw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" /> */}
 
                         <img className="m-auto my-2 pt-2 w-[10rem] rounded-full" src={user.avatar} alt="" />
